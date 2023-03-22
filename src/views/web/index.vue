eded718b3072419dcc4268fd26328b7962d2b122
fengfeng <fengfengzhidao@126.com>
2023-03-23 00:09:20 +0800

    广告组件

--- src/views/web/index.vue
+++ src/views/web/index.vue
@@ -9,7 +9,8 @@
           <GVBArticleCalendarCard style="margin-top: 20px"></GVBArticleCalendarCard>
         </div>
         <div class="right">
-          <GVBTagsCard></GVBTagsCard>
+          <GVBAdvertCard></GVBAdvertCard>
+          <GVBTagsCard style="margin-top: 20px"></GVBTagsCard>
           <GVBSiteCard style="margin-top: 20px"></GVBSiteCard>
           <GVBFadeBackCard style="margin-top: 20px"></GVBFadeBackCard>
         </div>
@@ -28,6 +29,7 @@ import GVBArticleCalendarCard from "@/components/index/gvb_article_calendar.vue"
 import GVBTagsCard from "@/components/index/gvb_tag_card.vue"
 import GVBSiteCard from "@/components/index/gvb_site_card.vue"
 import GVBFadeBackCard from "@/components/index/gvb_fade_back_card.vue"
+import GVBAdvertCard from "@/components/index/gvb_advertis_card.vue"
 </script>
 
 <style lang="scss">
